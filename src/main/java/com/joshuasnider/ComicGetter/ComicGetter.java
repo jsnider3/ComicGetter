33f7443d90e326d83d0be95adc60c6e576012df2
Josh Snider <Joshua Snider>
2018-05-17 09:03:49 -0400

    Removed unused imports.

--- src/main/java/com/joshuasnider/ComicGetter/ComicGetter.java
+++ src/main/java/com/joshuasnider/ComicGetter/ComicGetter.java
@@ -16,9 +16,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Iterator;
 
 public abstract class ComicGetter implements Iterable<String> {
